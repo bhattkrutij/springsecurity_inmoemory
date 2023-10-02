f9440827315d33cffbb86d9bcc8ce621d2b4f320
DELL <krutibhatt.1231@gmail.com>
2023-10-02 15:20:45 +0530

    inital commit

---
+++ src/main/java/com/example/inmoemorysecurity/InmoemorysecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.inmoemorysecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InmoemorysecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InmoemorysecurityApplication.class, args);
+	}
+
+}
